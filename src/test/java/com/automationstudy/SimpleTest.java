3f25e643670d01401552d115f33530d10d048baf
Dmitriy <Valiev@lanit.ru>
2025-07-24 21:34:44 +0300

    Commit

---
+++ src/test/java/com/automationstudy/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.automationstudy;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    @Test
+    public void myFirstTest() {
+
+        System.out.println("Мой первый TestNG тест запущен!");
+
+    }
+}
