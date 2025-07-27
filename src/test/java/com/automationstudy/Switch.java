0a0264807f9a2c84efa099f102e0e007dc75b722
Dmitriy <Valiev@lanit.ru>
2025-07-27 21:05:13 +0300

    commit

---
+++ src/test/java/com/automationstudy/Switch.java
@@ -0,0 +1,74 @@
+package com.automationstudy;
+
+public class Switch {
+    public static void main(String[] args) {
+        /*
+        String color = "blue";
+        switch (color) {
+            case "red":
+                System.out.println("Вы выбрали красный");
+                break;
+            case "green":
+                System.out.println("Вы выбрали зеленый");
+                break;
+            case "blue":
+                System.out.println("Вы выбрали синий");
+                break;
+            default:
+                System.out.println("Неизвестный цвет");
+                break;
+        }
+         */
+        /*
+        int day = 8; // 1-5 будни, 6-7 выходные
+
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Это будний день.");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Это выходной день.");
+                break;
+            default:
+                System.out.println("Неверный номер дня.");
+                break;
+        }
+         */
+                int month = 4; // Попробуй разные значения: 1, 4, 12, 13
+                String season;
+
+                switch (month) {
+                    case 12:
+                    case 1:
+                    case 2:
+                        season = "Зима";
+                        break;
+                    case 3:
+                    case 4:
+                    case 5:
+                        season = "Весна";
+                        break;
+                    case 6:
+                    case 7:
+                    case 8:
+                        season = "Лето";
+                        break;
+                    case 9:
+                    case 10:
+                    case 11:
+                        season = "Осень";
+                        break;
+                    default:
+                        season = "Неизвестный месяц";
+                        break;
+                }
+                System.out.println("Месяц " + month + " относится к сезону: " + season);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/automationstudy/uslov.java
@@ -0,0 +1,28 @@
+package com.automationstudy;
+
+public class uslov {
+    public static void main(String[] args) {
+      /*  int age = 12;
+        if (age >= 20) {
+            System.out.println("Вы взрослый");
+        }
+        else if (age >= 13) {
+            System.out.println("Вы подросток");
+        }
+        else if (age < 13) {
+            System.out.println("Вы ребенок");
+        }
+       */
+                int score = 85;
+
+                if (score >= 90) {
+                    System.out.println("Excellent score!");
+                } else if (score >= 75) {
+                    System.out.println("Good score.");
+                } else if (score >= 50) {
+                    System.out.println("Passed.");
+                } else {
+                    System.out.println("Failed.");
+                }
+            }
+        }
